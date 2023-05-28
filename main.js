21f4ddc8aabe9ff123d399c16393f23803476a6e
clauf <claufsts@gmail.com>
2023-05-28 20:37:12 -0300

    Integrando API RandonUsers

--- main.js
+++ main.js
@@ -4,7 +4,36 @@ import { createApp } from 'vue'
 createApp({
   data() {
     return {
-      sample: 'sample text'
+      contacts: [],
     }
+  },
+
+  methods: {
+    async loadContacts() {
+      const response = await fetch('https://randomuser.me/api/?results=15');
+      const { results } = await response.json();
+
+
+      results.forEach(model => {
+        const {name, email, picture, location:{ city }} = model;
+
+        const contact = {
+          picture: picture.large,
+          firstName: name.first,
+          lastName: name.last,
+          email,
+          city
+        }
+
+        console.log(contact)
+
+        this.contacts.push(contact);
+      });
+    }
+  },
+
+  async mounted() {
+    await this.loadContacts();
   }
+
 }).mount("#app");
